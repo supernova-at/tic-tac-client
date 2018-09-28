962bbf7faf2ba039bba9ddd5c4478eb125d02c96
Stephen Rugh <stephen.rugh@bazaarvoice.com>
2018-09-28 15:39:38 -0500

    The best strategy

--- players/8/index.mjs
+++ players/8/index.mjs
@@ -9,26 +9,26 @@ export default class Player {
   /**
    * IMPLEMENT: Given the current game state, return back
    * your desired move.
-   * 
+   *
    * @param {string} token Your team's token (X or O).
    *
    * @param {Array} gameState The current state of the game.
    *  The array [-, X, O, X, O, -, -, -, X] represents the following:
    *  - | X | O
    * -----------
-   *  X | O | - 
+   *  X | O | -
    * -----------
    *  - | - | X
    *
    * @returns {Number} The index into the array where you wish to place your mark:
    *  0 | 1 | 2
    * -----------
-   *  3 | 4 | 5 
+   *  3 | 4 | 5
    * -----------
    *  6 | 7 | 8
    */
   static makeMove(token, gameState) {
-    // Just pick the first open square.
-    return gameState.indexOf('-');
+    // Just pick the last open square.
+    return gameState.reverse().indexOf('-');
   }
 };
